7564fddd12701d18b522ee2f479cf361c6ef8015
Saymed <gasayagom@gmail.com>
2023-02-10 18:54:54 -0400

    Agregar archivos de proyecto.

---
+++ Prepa 03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prepa_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo_cuadrado = new Cuadrado(5, "azul");
+            Rectangulo rectangulo = new Rectangulo(2,3, "blanco");
+
+            Figura figura1 = rectangulo_cuadrado;
+            Figura figura2 = rectangulo;
+
+            Figura figura3 = new Circulo(2, "negro");
+
+            figura1.MostrarDatos();
+          
+            figura2.MostrarDatos();
+           
+            figura3.MostrarDatos();
+
+            Console.WriteLine($" Area {figura3.GetArea()}, Perimetro {figura3.GetPerimetro()}");
+
+            Console.ReadLine();
+           
+            
+        }
+    }
+}
